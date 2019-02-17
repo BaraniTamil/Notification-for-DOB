bcdf8faadaa8a0c0c37949b5297b9211bc98e0a5
BaraniTamil <47718972+BaraniTamil@users.noreply.github.com>
2019-02-17 22:24:37 +0530

    Add files via upload

---
+++ dq.php
@@ -0,0 +1,25 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "DOB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO com (sno,date)
+VALUES ('03','2019-03-25')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
